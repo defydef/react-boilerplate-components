cb7f36225df29cb79a3f2277e456da265bce344d
defy <8841170+defydef@users.noreply.github.com>
2023-09-24 21:14:33 +0700

    Add some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./index.css";
 import "./App.css";
 import ImageCarousell from "./pages/ImageCarousell";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="app">
-      <ImageCarousell />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="image-carousell" element={<ImageCarousell />} />
+      </Routes>
+    </BrowserRouter>
+    // <div className="app">
+    //   <ImageCarousell />
+    // </div>
   );
 }
 
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <div className="app">Hello</div>;
+}
+
+export default HomePage;
